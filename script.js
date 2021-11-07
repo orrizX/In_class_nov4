80f6b06775dd802a834c53ef23709ef84efa1311
orrizX <zo292@nyu.edu>
2021-11-07 15:58:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function displayResult() {
   document.getElementById("button").innerHTML = "Thanks for signing up!";
   }
+
+
+function ButtonClicked()
+{
+   document.getElementById("formsubmitbutton").style.display = "none"; // to undisplay
+   document.getElementById("buttonreplacement").style.display = ""; // to display
+   return true;
+}
+var FirstLoading = true;
+function RestoreSubmitButton()
+{
+   if( FirstLoading )
+   {
+      FirstLoading = false;
+      return;
+   }
+   document.getElementById("formsubmitbutton").style.display = ""; // to display
+   document.getElementById("buttonreplacement").style.display = "none"; // to undisplay
+}
+
+document.onfocus = RestoreSubmitButton;
+
+
